b5015f248a7f2f4daf66ad239c2a7117091ebe3c
yinna <yinna@aiit>
2023-10-30 15:03:25 +0800

    feat: 新增题3的解法

--- 3、字符串去掉次数最少的字符.js
+++ 3、字符串去掉次数最少的字符.js
@@ -1,10 +1,7 @@
 /**
  * 题目：
- *   实现删除字符串中出现次数最少的字符，若出现次数最少的字符有多个，则把出现次数最少的字符都删除。输出删除这些单词后的字符串，字符串中其它字符保持原来的顺序。
- * 输入描述：
- *   字符串只包含小写英文字母, 不考虑非法输入，输入的字符串长度小于等于20个字节。
- * 输出描述：
- *   删除字符串中出现次数最少的字符后的字符串。
+ *   去除字符串中出现次数最少的字符，不改变原字符串的顺序。
+ *   实现删除字符串中出现次数最少的字符，若出现次数最少的字符有多个，则把出现次数最少的字符都删除。输出删除这些单词后的字符串，字符串中其它字符保持原来的顺序
  * 示例:
  *   输入：ababac
  *   输出：ababa
@@ -12,3 +9,39 @@
  *   输入：aaabbbcceeff
  *   输出：aaabbb
 */
+
+function delMinCountStr(str) {
+    let strArr = str.split('')
+    // 先将字符串及其对应的次数做一个映射
+    let strCountObj = {}
+    strArr.forEach(element => {
+        if (!!strCountObj[element]) {
+            strCountObj[element]++
+        } else {
+            strCountObj[element] = 1
+        }
+    });
+
+    // 然后将最小的次数取出，循环原字符串数组，取出要删除的字符串
+    let minCount = Math.min(...Object.values(strCountObj))
+    let delStrArr = []
+    for (let i in strCountObj) {
+        if (strCountObj[i] === minCount) {
+            delStrArr.push(i)
+        }
+    }
+
+    // 最后将字符串删除
+    let resStr = JSON.parse(JSON.stringify(str))
+    strArr.forEach((element, index) => {
+        if (delStrArr.includes(element)) {
+            resStr = resStr.replace(element, '')
+        }
+    });
+
+    return resStr
+}
+
+let a = 'aaabbbcceeff'
+let res = delMinCountStr(a)
+console.log('最后的结果', res)
